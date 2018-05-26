f159ea38f17912fbb8182e25fd28cec86cea2a62
Fakhrulhilal Maktum <fakhrulhilal@gmail.com>
2018-05-26 19:42:10 +0700

    fix(build): update tfx to use local bin folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,20 @@ var shell = require('shelljs'),
     path = require('path'),
     fs = require('fs'),
     runSequence = require('run-sequence'),
-    hashsum = require('gulp-hashsum');
+    hashsum = require('gulp-hashsum'),
+    minimist = require('minimist');
 
-var _build = 'build',
-    manifest = 'vss-extension.json',
-    checksumAlgorithm = 'sha1';
+var _build = path.resolve('build'),
+	manifest = path.resolve('vss-extension.json'),
+	tfx = path.join(path.resolve('node_modules'), '.bin', 'tfx'),
+    options = minimist(process.argv.slice(2), {
+        string: 'token',
+        string: 'hash',
+        default: {
+            token: null,
+            hash: 'sha1'
+        }
+    });
 
 gulp.task('clean', function() {
     return shell.rm('-rf', _build);
@@ -42,23 +51,23 @@ function ensureExtensionExists(dir) {
 gulp.task('package', function() {
     if (!fs.existsSync(_build))
         shell.mkdir('-p', _build);
-    var cmd = 'tfx extension create --manifest-globs ' + manifest + ' --output-path ' + _build;
+    var cmd = tfx + ' extension create --manifest-globs ' + manifest + ' --output-path ' + _build;
     executeVerboseCommand(cmd);
 });
 
 gulp.task('publish', function() {
     var extension = ensureExtensionExists(_build);
-    var cmd = 'tfx extension publish --vsix ' + path.join(_build, extension) + ' --token "' + process.env['VSS.Token'] + '"';
+    var cmd = tfx + ' extension publish --vsix ' + path.join(_build, extension) + ' --token "' + options.token + '"';
     executeVerboseCommand(cmd);
 });
 
 gulp.task('hash', function () {
     var extension = ensureExtensionExists(_build);
-    var checksumFile = path.basename(extension) + '.' + checksumAlgorithm;
+    var checksumFile = path.basename(extension) + '.' + options.hash;
     console.log('creating checksum file:', checksumFile);
     gulp.src([path.join(_build, '*.vsix')])
         .pipe(hashsum({ 
-            hash: 'sha1',
+            hash: options.hash,
             force: true,
             dest: _build,
             filename: checksumFile,
